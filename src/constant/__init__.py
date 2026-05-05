bbd5aee79cee77df2b1f7ab8b9e4e88ed5f64b5f
Taherali <taherali.535251@gmail.com>
2026-05-05 21:43:36 +0530

    added modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "pwskills"
+MONGO_COLLECTION_NAME = "waferfault"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL ="mon"
+
+MODEL_FILE_NAME ="model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
